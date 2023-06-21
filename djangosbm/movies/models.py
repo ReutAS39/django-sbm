e97e193b888d9ea460ab8cfd83fad1f75f3d5730
ReutAS <reutas39@gmail.com>
2023-06-21 16:40:48 +0200

    mvc upd

--- djangosbm/movies/models.py
+++ djangosbm/movies/models.py
@@ -5,6 +5,9 @@ from django.urls import reverse
 class Genre(models.Model):
     name = models.CharField(max_length=255, unique=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Actor(models.Model):
     name = models.CharField(max_length=255, unique=True)
--- djangosbm/movies/views.py
+++ djangosbm/movies/views.py
@@ -8,6 +8,11 @@ class FilmList(ListView):
     template_name = 'film_list.html'
     context_object_name = 'film_list'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # context['genres'] = context['film_list'].genre.all()
+        return context
+
 class FilmDetail(DetailView):
     model = Film
     template_name = 'film.html'
