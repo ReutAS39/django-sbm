b4fd66adedf6301dba26f9adf888e176e1ccff7e
ReutAS <reutas39@gmail.com>
2023-06-26 16:24:55 +0200

    mvt upd

--- djangosbm/actor/views.py
+++ djangosbm/actor/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views.generic import CreateView, UpdateView, DetailView
 
 from actor.forms import PersonForm
-from movies.models import Person
+from movies.models import Person, FilmActor
 
 
 class PersonDetail(DetailView):
@@ -14,7 +14,8 @@ class PersonDetail(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['genres'] = context['person'].genres.all()
-        # context['actors'] = context['film'].actor.all()
+        # print(self.request.POST.get)
+        # context['films'] = FilmActor.objects.filter(person_id=self.request.film_id)
 
         return context
 
