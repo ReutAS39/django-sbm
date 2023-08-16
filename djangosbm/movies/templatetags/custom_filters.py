7f74b3b742dd247cf8de6de8d79db1e4fb3472fc
ReutAS <reutas39@gmail.com>
2023-08-16 16:25:46 +0200

    filter added

--- djangosbm/movies/templatetags/custom_filters.py
+++ djangosbm/movies/templatetags/custom_filters.py
@@ -4,6 +4,6 @@ register = template.Library()
 
 @register.filter()
 def digit(value):
-    n = str(value)[::-1]
-    b = ' '.join(n[i:i + 3] for i in range(0, len(n), 3))[::-1]
-    return b
+    return '{0:,}'.format(value).replace(',', ' ')
+
+
